5ec83caaa57645c5f4311f8bb8a90d5035859029
Andrei Shevtsov <andrei.shevtsov@blueapron.com>
2018-02-26 14:06:58 -0500

    factory_bot

--- spec/factories/controllers/request.rb
+++ spec/factories/controllers/request.rb
@@ -14,7 +14,7 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-FactoryGirl.define do
+FactoryBot.define do
   factory :controller_request, class: Gruf::Controllers::Request do
     method_key { :get_thing }
     service { Rpc::ThingService::Service }
