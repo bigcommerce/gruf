eb5e345f8758d632ba02490e3dec20a6904c650c
Miller <miller@daangn.com>
2023-02-16 00:48:30 +0900

    Lint

--- lib/gruf/configuration.rb
+++ lib/gruf/configuration.rb
@@ -188,7 +188,7 @@ module Gruf
       }
       self.use_default_interceptors = ::ENV.fetch('GRUF_USE_DEFAULT_INTERCEPTORS', 1).to_i.positive?
 
-      if self.use_default_interceptors
+      if use_default_interceptors
         interceptors.use(::Gruf::Interceptors::ActiveRecord::ConnectionReset)
         interceptors.use(::Gruf::Interceptors::Instrumentation::OutputMetadataTimer)
       end
